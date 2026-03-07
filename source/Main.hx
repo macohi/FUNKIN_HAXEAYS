dff068383b21458fdcd723d5ed4bd46c85ef45f2
Macohi <sinconsistencia@gmail.com>
2026-03-07 15:46:30 -0600

    chore: skip splash

--- source/Main.hx
+++ source/Main.hx
@@ -6,6 +6,6 @@ import openfl.display.Sprite;
 class Main extends Sprite {
 	public function new() {
 		super();
-		addChild(new FlxGame(0, 0, InitState));
+		addChild(new FlxGame(0, 0, InitState, 60, 60, true));
 	}
 }
