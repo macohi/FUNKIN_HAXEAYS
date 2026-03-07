039acd30c61184139e5582b42376daf6fab34f02
Macohi <sinconsistencia@gmail.com>
2026-03-07 15:06:07 -0600

    chore: hxcpp for windows

--- libraries.bat
+++ libraries.bat
@@ -3,7 +3,9 @@ haxelib newrepo
 haxelib install flixel
 haxelib install flixel-animate
 
-haxelib install hscript
 haxelib install hscript-iris
 
 haxelib install formatter
+haxelib install hscript
+haxelib install hxcpp
+haxelib install hxcpp-debug-server
